f40924b44d1cc814d3dddc2de46165f2dcbbc08e
Kelin2025 <kelin2025@yandex.ru>
2018-02-11 02:44:17 +0300

    Updated adapter for newest Apicase

--- index.js
+++ index.js
@@ -1,49 +1,46 @@
-const pathToRegexp = require('path-to-regexp')
+var pathToRegexp = require('path-to-regexp')
 
-function compilePath (url, params) {
+function compilePath(url, params) {
   return pathToRegexp.compile(url)(params)
 }
 
-function evaluateHeaders (headers) {
-  return typeof headers === 'function'
-    ? headers()
-    : headers
-}
-
-function buildQueryString (query) {
+function buildQueryString(query) {
   var queryString = ''
-  Object.keys(query).forEach(function encodeQueryPart (key) {
-    queryString += encodeURIComponent(key) + '=' + encodeURIComponent(query[key])
+  Object.keys(query).forEach(function encodeQueryPart(key) {
+    queryString +=
+      encodeURIComponent(key) + '=' + encodeURIComponent(query[key])
   })
-  return queryString.length
-    ? '?' + queryString
-    : ''
+  return queryString.length ? '?' + queryString : ''
 }
 
-function fetchAdapter (ctx) {
-  var options = {
-    method: ctx.options.method || 'GET',
-    credentials: ctx.options.credentials || 'omit'
-  }
-  var url = compilePath(ctx.options.url, ctx.options.params || {})
-  if (ctx.options.body) options.body = ctx.options.body
-  if (ctx.options.headers) options.headers = evaluateHeaders(ctx.options.headers)
-  if (ctx.options.query) url += buildQueryString(ctx.options.query)
-
-  return fetch(url, options)
-    .then(function resolveFetch (res) {
-      if (res.ok) {
-        return res[ctx.options.parser || 'json']()
-      } else {
-        throw res
+module.exports = {
+  callback(ctx) {
+    return fetch(url, options)
+      .then(ctx.resolve)
+      .catch(ctx.reject)
+  },
+  convert(payload) {
+    var res = {
+      url: compilePath(payload.url, payload.params || {}),
+      options: {
+        method: payload.method || 'GET',
+        headers: payload.headers || {},
+        credentials: payload.createntials || 'omit'
       }
-    })
-    .then(function resolveAdapter (data) {
-      ctx.done(data)
-    })
-    .catch(function rejectAdapter (reason) {
-      ctx.fail(reason)
-    })
+    }
+    if (payload.query) {
+      res.url += buildQueryString(payload.query)
+    }
+    if (payload.body) {
+      res.options.body = payload.body
+    }
+    return res
+  },
+  merge(to, from) {
+    var res = Object.assign(from, to)
+    if (to.url && from.url) {
+      res.url = to.url[0] === '/' ? to.url : [from.url, to.url].join('/')
+    }
+    return res
+  }
 }
-
-module.exports = fetchAdapter
