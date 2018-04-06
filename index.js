e10263c25cce15032c4a460df74c97d961c90e79
Anton Kosykh <kelin2025@yandex.ru>
2018-04-06 17:02:18 +0300

    eslint --fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const fetch =
-  (typeof window === "object" && window.fetch) || require("node-fetch")
-const pathToRegexp = require("path-to-regexp")
+  (typeof window === 'object' && window.fetch) || require('node-fetch')
+const pathToRegexp = require('path-to-regexp')
 
 const parseUrl = url => {
-  let origin = ""
-  let pathname = ""
-  if (url.indexOf("://") > -1) {
-    const res = url.match("(^(?:(?:.*?)?//)?[^/?#;]*)(.*)")
+  let origin = ''
+  let pathname = ''
+  if (url.indexOf('://') > -1) {
+    const res = url.match('(^(?:(?:.*?)?//)?[^/?#;]*)(.*)')
     origin = res[1]
     pathname = res[2]
   } else {
@@ -21,23 +21,23 @@ const uriReducer = (res = [], [key, val]) =>
   res.concat(
     Array.isArray(val)
       ? val.reduce((res, val, i) => uriReducer(res, [`${key}[]`, val]), [])
-      : typeof val === "object"
+      : typeof val === 'object'
         ? Object.entries(val).reduce(
-            (res, [i, val]) => uriReducer(res, [`${key}[${i}]`, val]),
-            []
-          )
+          (res, [i, val]) => uriReducer(res, [`${key}[${i}]`, val]),
+          []
+        )
         : `${encodeURIComponent(key)}=${encodeURIComponent(val)}`
   )
 
-const withQuestion = res => (res.length && `?${res}`) || ""
+const withQuestion = res => (res.length && `?${res}`) || ''
 
 const buildQueryString = payload =>
   withQuestion(
-    typeof payload === "string"
+    typeof payload === 'string'
       ? payload
       : Object.entries(payload)
-          .reduce(uriReducer, [])
-          .join("&")
+        .reduce(uriReducer, [])
+        .join('&')
   )
 
 const defaultStatusValidator = status => status >= 200 && status < 300
@@ -45,7 +45,7 @@ const defaultStatusValidator = status => status >= 200 && status < 300
 const prepareBody = body =>
   body instanceof FormData
     ? body
-    : typeof body === "object" ? JSON.stringify(body) : body
+    : typeof body === 'object' ? JSON.stringify(body) : body
 
 const createResponse = res => body => ({
   status: res.status,
@@ -73,8 +73,8 @@ export default {
 
       const stream = res.clone()
 
-      const parser = payload.parser[isValid ? "done" : "fail"]
-      const callback = isValid ? "resolve" : "reject"
+      const parser = payload.parser[isValid ? 'done' : 'fail']
+      const callback = isValid ? 'resolve' : 'reject'
 
       return stream[parser]()
         .then(body => cbs[callback](responseWith(body)))
@@ -88,7 +88,7 @@ export default {
     return fetch(payload.url, payload.options)
       .then(done)
       .catch(function(err) {
-        if (err.name === "AbortError") return
+        if (err.name === 'AbortError') return
         fail(err)
       })
   },
@@ -104,15 +104,15 @@ export default {
     const res = {
       url: origin + compilePath(pathname, payload.params || {}),
       parser: (payload.parser &&
-        (typeof payload.parser === "string"
+        (typeof payload.parser === 'string'
           ? { done: payload.parser, fail: payload.parser }
-          : payload.parser)) || { done: "json", fail: "json" },
+          : payload.parser)) || { done: 'json', fail: 'json' },
       controller: controller,
       validateStatus: payload.validateStatus || defaultStatusValidator,
       options: {
-        method: payload.method || "GET",
+        method: payload.method || 'GET',
         headers: payload.headers || {},
-        credentials: payload.createntials || "omit"
+        credentials: payload.createntials || 'omit'
       }
     }
     if (payload.query) {
@@ -122,10 +122,10 @@ export default {
       res.options.body = (payload.prepareBody || prepareBody)(payload.body)
     }
     if (
-      typeof payload.body === "object" &&
+      typeof payload.body === 'object' &&
       !(payload.body instanceof FormData)
     ) {
-      res.options.headers["Content-Type"] = "application/json"
+      res.options.headers['Content-Type'] = 'application/json'
     }
     if (controller) {
       res.options.signal = controller.signal
@@ -136,7 +136,7 @@ export default {
   merge(from, to) {
     const res = Object.assign({}, from, to)
     if (to.url !== undefined && from.url !== undefined) {
-      res.url = to.url[0] === "/" ? to.url : [from.url, to.url].join("/")
+      res.url = to.url[0] === '/' ? to.url : [from.url, to.url].join('/')
     }
     return res
   }
